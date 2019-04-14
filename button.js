ee48122a87f674a4798f97fb3ca7461702ba3685
Patryk <patryk-witczak1@wp.pl>
2019-04-14 20:19:08 +0200

    added cookies div and js

--- button.js
+++ button.js
@@ -29,3 +29,17 @@ function showInfo(pTags){
 function hideInfo(pTags){
     pTags.style.zIndex = "-5";
 }
+
+
+//////////cookies/////////////////////////////////////////////////////////////////////////////
+function WHCheckCookies(){
+  if(!localStorage.cookies_accepted) {
+    var cookies_message = document.getElementById("cookies-message"); 
+    cookies_message.style.display="block" 
+  }
+} 
+function WHCloseCookiesWindow(){ 
+  localStorage.cookies_accepted = true; 
+  document.getElementById("cookies-message-container").removeChild(document.getElementById("cookies-message"));
+}
+window.onload = WHCheckCookies;
