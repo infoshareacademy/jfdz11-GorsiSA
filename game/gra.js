1d74026a7307569d9bc3dd96b38dfbd754ecb4e2
Patryk <patryk-witczak1@wp.pl>
2019-04-14 20:04:03 +0200

    mechanizm gry

---
+++ game/gra.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+    const distanceMax = 600;
+    const distanceMin = 50;
+    let currentPosition = 0;
+    const obstacleWidth = 50;
+    const positions = [];
+    const gameNode = document.querySelector('#stage');
+    let intervalId;
+    for (let i = 0; i < 10; i += 1) {
+        const range = distanceMax - distanceMin
+        const increment = Math.random() * range + distanceMin;
+        const lastPosition = positions[positions.length - 1]
+        if (lastPosition === undefined) {
+            positions.push(increment);
+        } else {
+            positions.push(lastPosition + increment);
+        }
+    }
+
+    console.log(positions)
+    positions.forEach(position => {
+        const obstacle = document.createElement('div');
+        obstacle.classList.add('square');
+        obstacle.style.left = position + 'px';
+        gameNode.appendChild(obstacle);
+    })
+
+    gameNode.addEventListener('click', () => {
+        clearInterval(intervalId);
+        currentPosition = 0;
+        intervalId = setInterval(() => {
+            currentPosition = (currentPosition + 10) % 2000;
+            gameNode.style.marginLeft = -currentPosition + 'px';
+        }, 16)
+    })
+
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            console.log('space');
+        }
+    })
+})()
